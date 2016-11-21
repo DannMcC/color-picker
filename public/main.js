5fe6cfe99a7db32c19d7386dd7e67a4f9ff8a616
Dan McCune <megadannn@gmail.com>
2016-11-21 16:04:23 -0500

    finished css and html working on js

--- public/main.js
+++ public/main.js
@@ -1,6 +1,33 @@
+let hue = 0
+let sat = 0
+let light = 0
+let alpha = 0
+
+const getHSL = () => {
+  return `hsl(${hue}, ${sat}%, ${light}% , ${alpha})`
+}
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const hueInput = document.querySelector('input[name=hue]')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    console.log(getHSL())
+  })
+  const satInput = document.querySelector('input[name=sat]')
+  satInput.addEventListener('input', () => {
+    sat = satInput.value
+    console.log(getHSL())
+  })
+  const lightInput = document.querySelector('input[name=light]')
+  lightInput.addEventListener('input', () => {
+    light = lightInput.value
+    console.log(getHSL())
+  })
+  const alphaInput = document.querySelector('input[name=alpha]')
+  alphaInput.addEventListener('input', () => {
+    alpha = alphaInput.value
+    console.log(getHSL())
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
