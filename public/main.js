a9e25a7babf8c18397a1ecaeeece289c362402f1
Dan McCune <megadannn@gmail.com>
2016-11-21 19:08:11 -0500

    deloying

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
-let hue = 0
-let sat = 0
-let light = 0
-let alpha = 0
+let hue = 100
+let sat = 50
+let light = 50
+let alpha = 0.5
 
 const getHSL = () => {
-  return `hsl(${hue}, ${sat}%, ${light}% , ${alpha})`
+  return `hsla(${hue}, ${sat}%, ${light}% , ${alpha})`
 }
 
 const main = () => {
@@ -12,22 +12,32 @@ const main = () => {
   hueInput.addEventListener('input', () => {
     hue = hueInput.value
     console.log(getHSL())
+    document.querySelector('.box').style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
   })
   const satInput = document.querySelector('input[name=sat]')
   satInput.addEventListener('input', () => {
     sat = satInput.value
     console.log(getHSL())
+    document.querySelector('.box').style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
   })
   const lightInput = document.querySelector('input[name=light]')
   lightInput.addEventListener('input', () => {
     light = lightInput.value
     console.log(getHSL())
+    document.querySelector('.box').style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
   })
   const alphaInput = document.querySelector('input[name=alpha]')
   alphaInput.addEventListener('input', () => {
     alpha = alphaInput.value
     console.log(getHSL())
+    document.querySelector('.box').style.backgroundColor = getHSL()
+    document.querySelector('h3').textContent = getHSL()
   })
+  document.querySelector('.box').style.backgroundColor = getHSL()
+  // document.querySelector('h3').textContent = getHSL()
 }
 
 document.addEventListener('DOMContentLoaded', main)
